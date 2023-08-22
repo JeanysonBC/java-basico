852e20f661de37925b980ce0231dbd1e9a2b3a69
JeanysonBC <jeanysonbc@gmail.com>
2023-08-22 19:23:56 -0300

    Código final projeto desafio prático ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //TODO  Conhecer e importar a classe 
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+        
+        scanner.nextLine();
+        
+        System.out.println("Digite o nome completo do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Informe o valor do saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
