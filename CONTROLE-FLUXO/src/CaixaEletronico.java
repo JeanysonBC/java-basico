da2aa9d3773e1cef1c5a5a348f3463d7472be5c1
JeanysonBC <jeanysonbc@gmail.com>
2023-08-22 23:26:29 -0300

    Exercicio de controle de fluxo com java

---
+++ CONTROLE-FLUXO/src/CaixaEletronico.java
@@ -0,0 +1,8 @@
+public class CaixaEletronico {
+    public static void main(String[] args) throws Exception {
+        int nota = 5;
+
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
